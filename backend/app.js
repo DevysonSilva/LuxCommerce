c5388b1ac87801f1621ae26e9d81880deddc2b1b
devysonsilva <devysonssilva@gmail.com>
2025-05-31 23:42:15 -0300

    update app.js

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Rota de teste
+app.get('/', (req, res) => {
+  res.send('LuxCommerce Backend is running!');
+});
+
+// Inicializar servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
