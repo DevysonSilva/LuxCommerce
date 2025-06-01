3a143f611dea734ef6b3de420200657049e9244a
devysonsilva <devysonssilva@gmail.com>
2025-06-01 14:02:54 -0300

    update adminRoutes.js

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -7,8 +7,5 @@ router.post("/registro", adminController.registrarAdmin);
 router.post("/login", adminController.loginAdmin);
 module.exports = router;
 
-// Importar controller, se não já tiver
-const adminController = require('../controllers/adminController');
-
 // Rota do painel
 router.get('/dashboard', adminController.dadosDashboard);
